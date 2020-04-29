bfa6301ace3bbd7e9e2e092d8c613742a1871757
Xuesong Liu <liuxs0@gmail.com>
2020-04-29 17:03:08 +0800

    easier

--- src/compile.sh
+++ src/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gcc -DTRAINING=1 -Wall -W -O3 -O0 -g -Wunused-variable -I../include denoise16.c kiss_fft.c pitch.c celt_lpc.c rnn.c rnn_data.c -o /media/yongyug/DATA/new_train/work/denoise_training -lm
+gcc -DTRAINING=1 -Wall -W -O3 -O0 -g -Wunused-variable -I../include denoise.c kiss_fft.c pitch.c celt_lpc.c rnn.c rnn_data.c -o denoise_training -lm
